09ec9506d9a1eb9c777f6fb521369f08c90b674a
Shaun James <ShaunDaneJames@Gmail.com>
2020-07-29 09:07:52 -0600

    Adds and passes has name

--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -9,5 +9,10 @@ class VendorTest < Minitest::Test
     vendor = Vendor.new("Rocky Mountain Fresh")
     assert_instance_of Vendor, vendor
   end
+
+  def test_it_has_a_name
+    vendor = Vendor.new("Rocky Mountain Fresh")
+    assert_equal "Rocky Mountain Fresh", vendor.name
+  end
   
 end
